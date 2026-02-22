d98401994465760f3d0df83358a1ea4265bd9f1f
Lennon Araujo <lennonaraujo15@gmail.com>
2026-02-22 00:28:45 -0300

    Somar Progam
    Program for adding numbers

---
+++ exercicios/src/fundamentos/somaDeNumeros.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class somaDeNumeros {
+
+    public static void main(String[] args) {
+        
+        int a, b, c;
+        a = 2;
+        b = 3;
+        c = a+b;
+
+        System.out.printf("Resultado: %d", c);
+    }
+
+}
