07815935fd25c027f31997dff138a322acaaa445
Lennon Araujo <lennonaraujo15@gmail.com>
2026-02-22 19:19:05 -0300

    ConversaoStringNumber

---
+++ exercicios/src/fundamentos/Convercoes.java
@@ -0,0 +1,41 @@
+package fundamentos;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Convercoes {
+    public static void main(String[] args) {
+        
+        //Conversao de Numero para String
+
+        /*
+
+        Integer num1 = 10000;
+        System.out.println(num1.toString().length());
+
+        int num2 = 20000;
+        System.out.println(Integer.toString(num2));
+        
+        */
+
+        //Conversao de String para Numero
+
+        String valor1 = JOptionPane.showInputDialog("Digite o primeiro numero");
+        String valor2 = JOptionPane.showInputDialog("Digite o segundo numero");
+        
+        System.out.println(valor1 + valor2);
+
+        double numero1 = Double.parseDouble(valor1);
+        double numero2 = Double.parseDouble(valor2);
+
+        double soma = numero1 + numero2;
+
+        System.out.println(soma);
+
+        //BigDecimal para mais precisao
+ 
+    }
+
+}
