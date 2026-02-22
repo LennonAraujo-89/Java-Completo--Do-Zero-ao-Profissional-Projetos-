80804f5a97a628cb7b67766082fbb5718975a8de
Lennon Araujo <lennonaraujo15@gmail.com>
2026-02-22 17:51:21 -0300

    Convencao Tipos Primitivos

---
+++ exercicios/src/fundamentos/ConvencaoDeTiposPrimitivos.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+public class ConvencaoDeTiposPrimitivos {
+
+    public static void main(String[] args) {
+        
+        double a = 1;
+        System.out.println(a);
+
+        float b = (float) 1.0;
+        System.out.println(b);
+
+        double c = 1.123456789;
+        System.out.println(c);
+
+        float d = (float) 1.123456789;
+        System.out.println(d);
+
+
+
+    }
+
+}
