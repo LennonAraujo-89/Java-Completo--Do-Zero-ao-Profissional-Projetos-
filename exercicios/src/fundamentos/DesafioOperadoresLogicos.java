b8979fb530dca1ddd052b93373e1d597be8fbef4
Lennon Araujo <lennonaraujo15@gmail.com>
2026-02-24 12:53:29 -0300

    AtualizacaoDoDesafio

--- exercicios/src/fundamentos/DesafioOperadoresLogicos.java
+++ exercicios/src/fundamentos/DesafioOperadoresLogicos.java
@@ -8,6 +8,7 @@ public class DesafioOperadoresLogicos {
         boolean trabalhoQ = false;
         boolean soverte = true;
         String tomaram = "";
+        boolean ganho;
 
         if (trabalhoQ == false && trabalhoT == false) {
             soverte = (!soverte);
@@ -27,6 +28,26 @@ public class DesafioOperadoresLogicos {
             System.out.println("Familia saiu para comprar TV 32 polegadas e " + tomaram);
         }else if (trabalhoT == false && trabalhoQ == false) {
             System.out.println("Familia não saiu e " + tomaram);
+        }
+
+        System.out.println("\n");
+
+        if (trabalhoT || trabalhoQ) {
+            ganho = trabalhoT || trabalhoQ;
+            System.out.println("Ganhou os dois ou pelo menos um!!! " + ganho);
+        }else {
+            ganho = trabalhoT || trabalhoQ;
+            System.out.println("Não ganhou nenhum!!! " + ganho);
+        }
+
+        System.out.println("\n");
+
+        if (trabalhoT ^ trabalhoQ) {
+            ganho = trabalhoT ^ trabalhoQ;
+            System.out.println("Um dia, deu certo!!! " + ganho);
+        }else {
+            ganho = trabalhoT ^ trabalhoQ;
+            System.out.println("só podia ganhar um dia ou não ganho nenhum!!! " + ganho);
         }
 
     }
