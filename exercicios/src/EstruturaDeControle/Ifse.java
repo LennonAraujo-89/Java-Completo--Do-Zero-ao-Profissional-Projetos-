b34c7189eaf7f8eb86965b5fd5417b438899ddcf
Lennon Araujo <lennonaraujo15@gmail.com>
2026-02-26 05:18:30 -0300

    IfSe

--- exercicios/src/EstruturaDeControle/Ifse.java
+++ exercicios/src/EstruturaDeControle/Ifse.java
@@ -1,8 +1,19 @@
 package EstruturaDeControle;
 
+import javax.swing.JOptionPane;
+
 public class Ifse {
     public static void main(String[] args) {
         
+       String valor = JOptionPane.showInputDialog("Informe o numero:");
+       int numero = Integer.parseInt(valor.trim());
+
+       if (numero % 2 == 0) {
+        System.out.println("Numero par!");
+       }else {
+        System.out.println("Numero impar!");
+       }
+
     }
 
 }
