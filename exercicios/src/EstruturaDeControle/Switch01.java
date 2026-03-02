28cb376e8d6c5c672c1a3f981565535447db7eb6
Lennon Araujo <lennonaraujo15@gmail.com>
2026-03-02 10:21:46 -0300

    Switch

---
+++ exercicios/src/EstruturaDeControle/Switch01.java
@@ -0,0 +1,17 @@
+package EstruturaDeControle;
+
+public class Switch01 {
+    public static void main(String[] args) {
+
+        switch (args) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+        
+    }
+
+}
