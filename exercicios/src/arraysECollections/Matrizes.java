da68921dafa58450cc5558dce7f4e9e7bfee0301
Lennon Araujo <lennonaraujo15@gmail.com>
2026-03-17 16:55:34 -0300

    Add Matrizes example for student grades
    Add a new Java example (Matrizes) that prompts for number of students and grades per student, fills a 2D double array with the entered grades and accumulates a total. Adds source file exercicios/src/arraysECollections/Matrizes.java and its compiled class exercicios/bin/arraysECollections/Matrizes.class. Note: the total is calculated but not yet printed.

---
+++ exercicios/src/arraysECollections/Matrizes.java
@@ -0,0 +1,32 @@
+package arraysECollections;
+
+import java.util.Scanner;
+
+public class Matrizes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos alunos? ");
+        int qtdeAlunos = sc.nextInt();
+
+        System.out.println("Quantas notas por aluno? ");
+        int qtdeNotass = sc.nextInt();
+
+        double[][] notasTurma = new double[qtdeAlunos][qtdeNotass];
+
+
+        double total = 0;
+        // a percorre cada aluno e n percorre cada nota
+        for(int a = 0; a < notasTurma.length; a++){
+            for(int n = 0; n < notasTurma[a].length; n++){
+                System.out.printf("Informe a nota %d do aluno %d: ", a + 1, n + 1);
+                notasTurma[a][n] = sc.nextDouble();
+                total += notasTurma[a][n];
+            }
+        }
+
+
+        sc.close();
+    }
+
+}
